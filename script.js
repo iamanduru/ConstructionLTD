ddcc4bbfc3b3fc115a504abd9547c9873882b580
Sharon Nrangi <sharonnarangi@gmail.com>
2025-02-01 09:49:51 +0300

    GRIND!!!

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-/*SHOW MENU*/
-const navMenu =  document.getElementById('nav-menu');
+const navMenu = document.getElementById('nav-menu');
 const navToggle = document.getElementById('nav-toggle');
 const navClose = document.getElementById('nav-close');
 
-/*Show Menu*/
-if(navToggle){
-    navToggle.addEventListener('click', () => {
-        navMenu.classList.add('show-menu');
-        });
+/* Show Menu */
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+  });
 }
 
-/*Menu Hide*/
-if(navClose){
-    navClose.addEventListener('click', () => {
-        navMenu.classList.remove('show-menu');
-    })
+/* Hide Menu */
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+  });
 }
-
 
     
